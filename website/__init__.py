673dace32dd6753c1d4a8413824ec1efc01246ae
rkekesi22 <rolcsi0194@gmail.com>
2021-10-01 11:17:45 +0200

    Minimal Application

---
+++ website/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+
+
+def create_app():
+    app = Flask(__name__)
+
+    # https://stackoverflow.com/questions/34902378/where-do-i-get-a-secret-key-for-flask
+    # Get the random string for secret key:
+    # >>> import secrets
+    # >>> secrets.token_urlsafe(16) OR secrets.token_hex(16)
+
+    # The secret key is needed to keep the client-side sessions secure.
+    app.secret_key = 'yhGT3ylGryGTCasbc7yoZQ'
+
+    return app
+
