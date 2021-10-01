45d9cedb0fa9f1172850606e18e64c97d6dda625
rkekesi22 <rolcsi0194@gmail.com>
2021-10-01 22:00:53 +0200

    SQLALchemy + Password security + login-user

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
 
-
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
@@ -13,6 +16,11 @@ def create_app():
     # The secret key is needed to keep the client-side sessions secure.
     app.secret_key = 'yhGT3ylGryGTCasbc7yoZQ'
 
+    # To Database
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
+
     from .views import views
     from .auth import auth
 
@@ -20,5 +28,15 @@ def create_app():
     app.register_blueprint(views)
     app.register_blueprint(auth)
 
+    from .models import User
+
+    create_database(app)
+
     return app
+
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
 
