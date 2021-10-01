1ed48b9be4772ad4c4bf386a3cf550af723088aa
rkekesi22 <rolcsi0194@gmail.com>
2021-10-01 22:18:29 +0200

    BASIS Template--->Innen kezdődik a kemény munka

--- website/__init__.py
+++ website/__init__.py
@@ -1,36 +1,41 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
+
 def create_app():
+    # A Flask osztály egy példánya
     app = Flask(__name__)
 
-    # https://stackoverflow.com/questions/34902378/where-do-i-get-a-secret-key-for-flask
-    # Get the random string for secret key:
-    # >>> import secrets
-    # >>> secrets.token_urlsafe(16) OR secrets.token_hex(16)
+    # https://flask.palletsprojects.com/en/1.0.x/quickstart/#sessions
+    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
 
-    # The secret key is needed to keep the client-side sessions secure.
-    app.secret_key = 'yhGT3ylGryGTCasbc7yoZQ'
-
-    # To Database
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
-
+    # Blueprint
     from .views import views
     from .auth import auth
 
-    # Registering Blueprints
-    app.register_blueprint(views)
-    app.register_blueprint(auth)
+    # Import and register the blueprint from the factory
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
 
-    from .models import User
+    from .models import User, Note
 
     create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
 
@@ -39,4 +44,3 @@ def create_database(app):
     if not path.exists('website/' + DB_NAME):
         db.create_all(app=app)
         print('Created Database!')
-
