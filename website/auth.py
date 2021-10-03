c6d19b6615542142331d9b03c35756858200a2ad
rkekesi22 <rolcsi0194@gmail.com>
2021-10-03 15:52:45 +0200

    Jelszó ellenörző

--- website/auth.py
+++ website/auth.py
@@ -27,20 +27,45 @@ def sign_up():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
+        # ÖTLET: Jelszó ellenőrzés: tartalmaz-e betűt,számot és egyéb karaktert
+
         # https://flask.palletsprojects.com/en/2.0.x/patterns/flashing/?highlight=flash
-        if len(email) < 4:
-            flash('Túl rövid email cím! Kérlek 3 karakternél többet adj meg.', category='error')
-        elif len(firstName) < 2:
+        if len(firstName) < 2:
             flash('Túl rövid vezetéknév! Kérlek 1 karakternél többet adj meg.', category='error')
         elif len(lastName) < 2:
             flash('Túl rövid keresztnév! Kérlek 1 karakternél többet adj meg.', category='error')
-        elif password1 != password2:
-            flash('A jelszavak nem egyeznek, kérlek próbáld meg újból.', category='error')
+        elif len(email) < 4:
+            flash('Túl rövid email cím! Kérlek 3 karakternél többet adj meg.', category='error')
         elif len(password1) < 5:
             flash('Túl rövid jelszó! Kérlek 4 karakternél többet adj meg.', category='error')
+        elif not password_control(password1):
+            flash('A jelszó helytelen!', category='error')
+        elif password1 != password2:
+            flash('A jelszavak nem egyeznek, kérlek próbáld meg újból.', category='error')
         else:
             flash('Sikeres regisztráció!', category='success')
 
 
 
     return render_template('sign_up.html')
+
+
+# Jelsó ellenőrzés
+def password_control(jelszo):
+    betu = False
+    szam = False
+    egyeb = False
+    # k, mint karakter
+    for k in jelszo:
+        if k.isspace():
+            return False
+        elif k.isalnum():
+            betu = True
+            szam = True
+        elif not k.isalnum():
+            egyeb = True
+
+    if betu and szam and egyeb:
+        return True
+
+    return False
