da558f9d1f8f4b2f9c974c2176ba180707412f65
rkekesi22 <rolcsi0194@gmail.com>
2021-10-04 11:37:50 +0200

    Új feladat html

--- website/views.py
+++ website/views.py
@@ -7,3 +7,8 @@ views = Blueprint('views',__name__,url_prefix='/')
 @login_required
 def home():
     return render_template('home.html', user = current_user)
+
+
+@views.route('/newtask')
+def newtask():
+    return render_template('newtask.html', user = current_user)
