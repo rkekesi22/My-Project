f6446057e64ca8dad3b91e561b7913c5d3388c93
rkekesi22 <rolcsi0194@gmail.com>
2021-10-04 14:52:38 +0200

    Nem tudom mi történt már de felhúzott és ezért feltöltöm mert működik

--- website/models.py
+++ website/models.py
@@ -1,9 +1,11 @@
 from . import db
 from flask_login import UserMixin
 
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer,primary_key=True)
     last_name = db.Column(db.String(60), unique=True)
     first_name = db.Column(db.String(60), unique=True)
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(120))
+
--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template,request
 from flask_login import login_required, current_user
 
 views = Blueprint('views',__name__,url_prefix='/')
@@ -9,6 +9,20 @@ def home():
     return render_template('home.html', user = current_user)
 
 
-@views.route('/newtask')
+@views.route('/newtask', methods=['GET', 'POST'])
 def newtask():
+    if request.method == 'POST':
+        project_name = request.form.get('projectName')
+        task_name = request.form.get('taskName')
+        status = request.form.get('status')
+        task_time = request.form.get('taskTime')
+        description = request.form.get('description')
+
+    print(request.form.to_dict())
+
     return render_template('newtask.html', user = current_user)
+
+
+@views.route('/currenttasks')
+def currenttasks():
+    return render_template('currenttasks.html', user = current_user)
